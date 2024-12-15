0c08fa2963a87e109c1359bba144eafc5d507184
Alexey Storozhev <dark.storoj@gmail.com>
2024-12-15 00:27:02 +0000

    Enable hlsearch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
